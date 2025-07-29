80ccd51db43c185f61e36b52601d03f58e18c7c1
saisudheerkatta <saisudheerkatta@gmail.com>
2025-07-29 11:22:40 +0530

    new test.js file added

---
+++ test.js
@@ -0,0 +1,4 @@
+let message = "This is a string."; // Declares a mutable variable
+const PI = 3.14159; // Declares an immutable constant
+let count = 10; // Declares a number
+let isActive = true; // Declares a boolean
